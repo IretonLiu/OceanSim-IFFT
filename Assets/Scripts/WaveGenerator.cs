2da2fe5925bbc6ae167ff4a7f8327f541aec15d8
Iretonsama <43218442+Iretonsama@users.noreply.github.com>
2022-03-18 00:12:59 +0200

    h0 not working...

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -16,6 +16,7 @@ public class WaveGenerator : MonoBehaviour
 
     [Header("Other")]
     public Texture2D gaussianNoise;
+    public Texture2D gaussianNoise2;
     public ComputeShader initialSpectrumCompute; // h0(k) h0(-k)
     public ComputeShader fourierAmplitudeCompute;
 
@@ -24,8 +25,13 @@ public class WaveGenerator : MonoBehaviour
     public RenderTexture h0minusk_RenderTexture;
 
 
-    public Material matH0k;
-    public Material matH0minusk;
+    public RenderTexture hktDy; // height change, this is the h0 from the paper
+    public RenderTexture hktDx; // directional change
+    public RenderTexture hktDz; // directional change
+
+
+    public Material matVis;
+    // public Material matH0minusk;
 
 
     // MeshData meshData;
@@ -41,6 +47,8 @@ public class WaveGenerator : MonoBehaviour
     bool shouldUpdate;
     void Awake()
     {
+        GaussianNoiseTexture gnt = new GaussianNoiseTexture();
+        gaussianNoise2 = gnt.generateGaussianTexture(256, 256);
         shouldUpdate = true;
     }
     void OnValidate()
@@ -54,10 +62,13 @@ public class WaveGenerator : MonoBehaviour
         if (shouldUpdate)
         {
             genInitialSpectrum();
-            matH0k.SetTexture("_MainTex", h0k_RenderTexture);
-            matH0minusk.SetTexture("_MainTex", h0minusk_RenderTexture);
+            matVis.SetTexture("_MainTex", hktDy);
+            // matH0minusk.SetTexture("_MainTex", h0minusk_RenderTexture);
             shouldUpdate = false;
         }
+        calcFourierAmplitude();
+        matVis.SetTexture("_MainTex", hktDy);
+
         // if (h0k_RenderTexture != null && h0minusk_RenderTexture != null)
         // {
         //     Texture2D tex1 = new Texture2D(h0k_RenderTexture.width, h0k_RenderTexture.height, TextureFormat.RGB24, false);
@@ -86,12 +97,11 @@ public class WaveGenerator : MonoBehaviour
 
     void genInitialSpectrum()
     {
+
         MeshGenerator meshGenerator = FindObjectOfType<MeshGenerator>();
 
         // if (h0k_RenderTexture == null)
         createTexture(ref h0k_RenderTexture, meshGenerator.N, meshGenerator.M);
-
-        // if (h0minusk_RenderTexture == null)
         createTexture(ref h0minusk_RenderTexture, meshGenerator.N, meshGenerator.M);
 
 
@@ -107,15 +117,21 @@ public class WaveGenerator : MonoBehaviour
         initialSpectrumCompute.SetFloat("A", A);
 
         initialSpectrumCompute.SetTexture(initialSpectrumKernel, "GaussianNoise", gaussianNoise);
+        initialSpectrumCompute.SetTexture(initialSpectrumKernel, "GaussianNoise2", gaussianNoise2);
+
         initialSpectrumCompute.SetTexture(initialSpectrumKernel, "H0k", h0k_RenderTexture);
         initialSpectrumCompute.SetTexture(initialSpectrumKernel, "H0minusk", h0minusk_RenderTexture);
 
         initialSpectrumCompute.Dispatch(initialSpectrumKernel, meshGenerator.N / LOCAL_WORK_GROUP_X, meshGenerator.M / LOCAL_WORK_GROUP_Y, 1);
     }
 
-    void calcFourierAmplitdue()
+    void calcFourierAmplitude()
     {
         MeshGenerator meshGenerator = FindObjectOfType<MeshGenerator>();
+
+        createTexture(ref hktDy, meshGenerator.N, meshGenerator.M);
+        createTexture(ref hktDx, meshGenerator.N, meshGenerator.M);
+        createTexture(ref hktDz, meshGenerator.N, meshGenerator.M);
 
         int fourierAmplitudeKernel = fourierAmplitudeCompute.FindKernel("CSFourierAmplitude");
 
@@ -127,6 +143,12 @@ public class WaveGenerator : MonoBehaviour
 
         fourierAmplitudeCompute.SetTexture(fourierAmplitudeKernel, "H0k", h0k_RenderTexture);
         fourierAmplitudeCompute.SetTexture(fourierAmplitudeKernel, "H0minusk", h0minusk_RenderTexture);
+
+        fourierAmplitudeCompute.SetTexture(fourierAmplitudeKernel, "Hkt_dy", hktDy);
+        fourierAmplitudeCompute.SetTexture(fourierAmplitudeKernel, "Hkt_dx", hktDx);
+        fourierAmplitudeCompute.SetTexture(fourierAmplitudeKernel, "Hkt_dz", hktDz);
+
+        fourierAmplitudeCompute.Dispatch(fourierAmplitudeKernel, meshGenerator.N / LOCAL_WORK_GROUP_X, meshGenerator.M / LOCAL_WORK_GROUP_Y, 1);
     }
 
 
